4b5262351aba257093b98ee0b9d08b8f1324899c
sbprabhakar <sbprabhakar@gmail.com>
2026-08-11 10:46:37 +0000

    Saved ch01_hello_world.py (11-08-26_1616)

---
+++ ch01_hello_world.py
@@ -0,0 +1,8 @@
+print("Hello Python interpreter!")
+print("Hello world!")
+
+print("Command: python ch01-hello_world.py")
+print("Command to see python version: python")
+print("Python version: Python 3.12.1 (main, Mar 11 2026, 12:17:56) [GCC 13.3.0] on linux") 
+
+print("Hello Python world!")
