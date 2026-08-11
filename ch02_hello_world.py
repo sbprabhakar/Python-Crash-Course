3d430111a7e70e461df5466a8393f07fc2daf005
sbprabhakar <sbprabhakar@gmail.com>
2026-08-11 11:20:38 +0000

    Saved ch02_hello_world.py (11-08-26_1650)

---
+++ ch02_hello_world.py
@@ -0,0 +1,13 @@
+print("Hello Python World!")
+
+message = "Hello Python World!"
+print(message)
+
+message = "Hello Python Crash Course World!"
+print(message)
+
+#message = "Hello Python Crash Course reader!"
+#print(mesage)
+
+mesage = "Hello Python Crash Course reader!"
+print(mesage)
