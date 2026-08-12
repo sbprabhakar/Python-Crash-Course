ca44fd7b7fab5ac4991c26e594ab88def5419f01
sbprabhakar <sbprabhakar@gmail.com>
2026-08-12 09:48:43 +0000

    Saved ch02p08_string_2.py (12-08-26_1518)

--- ch02p08_string_2.py
+++ ch02p08_string_2.py
@@ -3,3 +3,13 @@ favorite_language = 'python '
 print(favorite_language)
 
 print(favorite_language.rstrip())
+
+##
+favorite_language = favorite_language.rstrip()
+print(favorite_language)
+
+###
+fav_lan = ' python '
+print(fav_lan.rstrip())
+print(fav_lan.lstrip())
+print(fav_lan.strip())
