b9a6b5d2f2d64ae35e35af538e5e5517b24cf161
sbprabhakar <sbprabhakar@gmail.com>
2026-08-13 06:38:51 +0000

    Saved ch02p12_comment.py (12-08-26_1208)

---
+++ ch02p12_comment.py
@@ -0,0 +1,11 @@
+# Say hello to everyone.
+print("Hello Python people!")
+
+#the zen of Python
+import this     #it prints “The Zen of Python” by Tim Peters. 
+
+#The Python community’s
+#philosophy is contained in “The Zen of Python” by Tim Peters. You can
+#access this brief set of principles for writing good Python code by enter-
+#ing import this into your interpreter. I
+
