7310a54bf563e77f68fd6597734c5954ed5af0c3
sbprabhakar <sbprabhakar@gmail.com>
2026-08-13 09:10:49 +0000

    Saved ch03p01_bicycles.py (13-08-26_1440)

--- chapter_03/ch03p01_bicycles.py
+++ chapter_03/ch03p01_bicycles.py
@@ -8,8 +8,13 @@ print(bicycles[2].upper())
 
 print(bicycles[1] + "  " + bicycles[3])
 #
+#Python has a special syntax for accessing the last element in a list. 
+#By asking for the item at index -1, Python always returns the last item in the list.
 print(bicycles[-1])
 print(bicycles[-1]+" "+bicycles[-2]+" "+bicycles[-3]+" "+bicycles[-4])
 #
 print(bicycles[-4]+" "+bicycles[0])
 ##print(bicycles[-4]+" "+bicycles[-5])    #ERROR: IndexError: list index out of range
+#
+message = "My first bicycle was a "+ bicycles[0].title()+ "."
+print(message)
