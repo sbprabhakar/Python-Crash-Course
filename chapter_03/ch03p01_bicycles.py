0583ffb7f54ec2557efda0419ef74ba9c8b4822d
sbprabhakar <sbprabhakar@gmail.com>
2026-08-13 08:47:42 +0000

    Saved ch03p01_bicycles.py (13-08-26_1417)

--- chapter_03/ch03p01_bicycles.py
+++ chapter_03/ch03p01_bicycles.py
@@ -1,1 +1,15 @@
+#List: A list is a collection of items in a particular order. 
+bicycles = ['trek', 'cannondale', 'redline', 'specialised']
+print(bicycles)
+#
+print(bicycles[0])
+print(bicycles[0].title())
+print(bicycles[2].upper())
 
+print(bicycles[1] + "  " + bicycles[3])
+#
+print(bicycles[-1])
+print(bicycles[-1]+" "+bicycles[-2]+" "+bicycles[-3]+" "+bicycles[-4])
+#
+print(bicycles[-4]+" "+bicycles[0])
+##print(bicycles[-4]+" "+bicycles[-5])    #ERROR: IndexError: list index out of range
