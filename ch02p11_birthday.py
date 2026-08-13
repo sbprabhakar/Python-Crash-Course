b8a7bb18eed59eff17488f43dc51bb2bde01f54f
sbprabhakar <sbprabhakar@gmail.com>
2026-08-13 06:19:36 +0000

    Saved ch02p11_birthday.py (13-08-26_1149)

--- ch02p11_birthday.py
+++ ch02p11_birthday.py
@@ -1,3 +1,7 @@
+#age = 23
+#message ="Happy " + age + "rd Birthday!"    #ERROR: type error
+#print(message)
+
 age = 23
-message ="Happy " + age + "rd Birthday!"    #ERROR
+message = "Happy " + str(age) + "rd Birthday!"
 print(message)
