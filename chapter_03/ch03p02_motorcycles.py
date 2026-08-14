19cba37f4a95c37c54e9ce2b39e977e7186c4ef3
sbprabhakar <sbprabhakar@gmail.com>
2026-08-14 05:27:12 +0000

    Saved ch03p02_motorcycles.py (14-08-26_1057)

---
+++ chapter_03/ch03p02_motorcycles.py
@@ -0,0 +1,16 @@
+##Changing, adding, and removing elements##
+
+#Modifying Elements in a List: 
+motorcycles =['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+#
+motorcycles[0]='ducati'
+print(motorcycles)
+
+##Adding Elements to a List##
+#appending Elements to the End of a List:
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles.append('ducati')
+print(motorcycles)
